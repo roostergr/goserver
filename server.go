4c6082ad501a2098e56144713fb0ab0928c1ac60
roostergr <roostergr@yahoo.com>
2022-02-26 13:30:52 +0200

    add context

--- server.go
+++ server.go
@@ -3,26 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "hello\n")
-}
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
 
-func headers(w http.ResponseWriter, req *http.Request) {
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
 
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
 	}
 }
 
 func main() {
 
 	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
-
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8080", nil)
 }
